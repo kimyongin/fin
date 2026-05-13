3eed786b8af59f8047280c2b9c8152e0fd442bad
kimyongin <kimyongin@users.noreply.github.com>
2026-05-13 21:53:10 +0000

    deploy: a8eb8b5105bd3e9e690f9b4b74422d67ce4958fb

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = "";
+export const SUPABASE_URL = "https://ubmtflglqudrvumepzij.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_Iytmfmx6dhuf6UCBz5z3MQ_ERMpllSW";
