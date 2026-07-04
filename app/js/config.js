f0ebdb6b70b278a273cce795aa901e044169d28c
yongin <kimyongin@gmail.com>
2026-07-04 16:48:47 +0900

    Remove legacy static app

--- app/js/config.js
+++
@@ -1,4 +0,0 @@
-// Local development config.
-// GitHub Actions regenerates this file from repository secrets during deploy.
-export const SUPABASE_URL = 'https://ubmtflglqudrvumepzij.supabase.co';
-export const SUPABASE_ANON_KEY = 'sb_publishable_Iytmfmx6dhuf6UCBz5z3MQ_ERMpllSW';
